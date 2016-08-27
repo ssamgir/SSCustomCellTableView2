5bcbd07feb9df9c20face3874822aebf4cb88651
ssamgir <amitbobade2007@gmail.com>
2016-08-27 13:16:50 +0530

    first commit

---
+++ testtableview/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  testtableview
+//
+//  Created by Amit Bobade on 27/08/16.
+//  Copyright © 2016 Sandesh Samgir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UITableViewDataSource, UITableViewDelegate >
+
+{
+    
+
+}
+
+
+@end
+
---
+++ testtableview/ViewController.m
@@ -0,0 +1,61 @@
+//
+//  ViewController.m
+//  testtableview
+//
+//  Created by Amit Bobade on 27/08/16.
+//  Copyright © 2016 Sandesh Samgir. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+{
+    NSArray *tableData;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+ 
+    // Do any additional setup after loading the view, typically from a nib.
+
+
+
+    tableData = [NSArray arrayWithObjects:@"Christ Redeemer- Brazil ", @"Great Wall of China- China", @"Machu Picchu -Peru",@"Petra- Jordan",@"Pyramid at Chichén Itzá - Yucatan Peninsula-Mexico", @"Roman Colosseum- Rome- Italy",@"Taj Mahal- Agra - India",   nil];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [tableData count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    cell.imageView.image=[UIImage imageNamed:@"christ_461.jpg"];
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    return cell;
+    
+    
+}
+
+
+@end
